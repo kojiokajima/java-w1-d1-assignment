622e8fe5375db02fb7855b518d4705e23737b3fe
wanko <kouji124@w4.dion.ne.jp>
2020-10-06 02:33:57 +0900

    first commit

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,15 @@
+public class MyFirstJavaProgram{
+
+    public static void main(String[] args) {
+
+        int num = 550;
+
+        System.out.println(num);
+
+        num = 567;
+
+        System.out.println(num);
+
+    } 
+
+}
